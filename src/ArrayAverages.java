956a1f493e27c3c82845d38ef07049df46349704
1CameronT <camerontoyufc@outlook.com>
2016-12-19 16:05:37 -0600

    Lab 6-2
    Finish Array Average

---
+++ src/ArrayAverages.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+
+/**
+ * Name:Cameron
+ * Teacher: Mr.Hardman
+ * Assignment Demo, Program #1
+ * Date Last Modified: 11/21/2016
+ */
+
+/**
+ * @author c.toy
+ *
+ */
+public class ArrayAverages {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner userInput = new Scanner(System.in);
+		
+		double[] student1 ={56.2, 65.7, 45.32, 89.213, 65, 76, 54, 98, 45, 87.2};
+		
+		double[] student2 = new double[10];
+		
+		
+		double average1 = 0;
+		double average2 = 0;
+		
+		System.out.println("Please enter ten marks to be averaged:");
+		
+		for(int i = 0; i < student2.length; i++){
+			System.out.print("Please enter a mark: ");
+			student2[i] = userInput.nextDouble();
+			
+		}
+			
+			average1 = calcAverage(student1);
+			average2 = calcAverage(student2);
+			
+			
+			if(average2 > average1){
+			
+			System.out.println("Your average is greater than student1's by " + (average2 - average1) + " points,"); 
+			
+		}else if(average2 <average1){
+			
+			System.out.println("Your average is less than student1's by " + (average1 - average2) + " points,");
+			
+		} else {
+			
+			System.out.println("Your average is equal to student 1's.");
+			
+		}
+	   
+			/**
+        * calcAverage will calculate the average of the student's marks
+        *
+        * @param the marks array
+        * @return will return students marks as a double
+        */
+
+		public static double calcAverage( double[] marks ){
+	    
+		double sum=0;
+		double average=0;
+			
+			
+		for(int i = 0; i < marks.length; i++){ 
+				sum += marks[i]; 
+				
+		} 
+			 		 
+		average = sum / marks.length; 
+					 
+		sum = 0; 
+			 		 
+		return average; 
+			 		 
+		}
+		
+		userInput.close();
+		
+		} 
+			 	 
+			 } 
